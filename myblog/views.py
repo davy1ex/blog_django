ab205597fce8716c594a5a22a4f90e7ab9df7ec7
davy1ex <dezy1ex@gmail.com>
2019-09-12 23:02:58 +0400

    refactoring

--- myblog/views.py
+++ myblog/views.py
@@ -5,3 +5,7 @@ from myblog.models import Article
 def index(request):
     articles = Article.objects.all()
     return render(request, "index.html", {"articles": articles})
+
+def article(request, id):
+    article = Article.objects.get(id=id)
+    return render(request, "article.html", {})
