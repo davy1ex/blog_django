ace89c602633bdfee141a0dc4a20c1fd00c7929a
davy1ex <dezy1ex@gmail.com>
2019-09-18 20:59:42 +0400

    adding user login

--- auth/views.py
+++ auth/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth import authenticate
+# from django.contrib.auth.models import User
+
 from .forms import UserForm
 
 
 def login(request):
+    if request.method == "POST":
+        data = request.POST.copy()
+        user = authenticate(username=data.get("username") , password=data.get("password"))
+        
+        if user is not None:
+            return HttpResponse("Hello", + user.username)
+
+    
     form = UserForm()
+
     return render(request, "auth/login.html", {"form": form})
